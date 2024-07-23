caa4e1d0b2fd68daaf6e14afeaac12f5b7449cdf
Andrew SungWoo Chun <andrewchun041@gmail.com>
2024-07-23 12:23:04 -0700

    add event listener for submit and call function addItem; add function addItem to create a new object based on the input and push/add it to the items array

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(e) {
+    // stop page from reload
+    e.preventDefault();
+
+    // select input with name=item and get it's value
+    const text = this.querySelector('[name=item]').value;
+    // create a new object with the value
+    const item = {
+        // text: 'text',
+        text,
+        done: false
+    }
+    // push/add the new object to the items list
+    items.push(item);
+
+    this.reset();
+}
+
+addItems.addEventListener('submit', addItem);
